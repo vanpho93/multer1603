aded558a6d7b730f4bd2c9a63714c179dcaabc58
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-11 20:43:46 +0700

    multer

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage });
+function fileFilter(req, file, cb) {
+    if (file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+        console.log('SAI DINH DANG FILE');
+    }
+}
+
+const limits = { fileSize: 100 * 1024 };
+
+const upload = multer({ storage, fileFilter, limits });
 
 const app = express();
 app.use(express.static('public'));
@@ -18,6 +29,13 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.listen(3000, () => console.log('Server started'));
 
+const avatarUpload = upload.single('avatar');
+
 app.get('/', (req, res) => res.render('home')); 
-app.post('/upload', upload.single('avatar'), (req, res) => res.send('THANH_CONG'));
+app.post('/upload', (req, res) => {
+    avatarUpload(req, res, err => {
+        if (err) return res.send(`LOI: ${err}`);
+        res.send('UPLOAD THANH CONG');
+    });
+});
 
