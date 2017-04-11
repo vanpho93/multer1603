9476e2561e848bd5d95577265c0a8a56304d8004
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-11 19:57:55 +0700

    storage

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './public'),
+    filename: (req, file, cb) => cb(null, '3.png')
+});
+
+const upload = multer({ storage });
 
 const app = express();
 app.use(express.static('public'));
@@ -8,3 +16,4 @@ app.set('views', './views');
 app.listen(3000, () => console.log('Server started'));
 
 app.get('/', (req, res) => res.render('home')); 
+app.post('/upload', upload.single('avatar'), (req, res) => res.send('THANH_CONG'));
